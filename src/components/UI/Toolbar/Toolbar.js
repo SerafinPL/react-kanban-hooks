120adb779b21c79bf39f288f6ac7688d6b0f5183
serafinpl <ser.burberibar@gmail.com>
2021-04-06 18:40:09 +0200

    starting making toolbar

--- src/components/UI/Toolbar/Toolbar.js
+++ src/components/UI/Toolbar/Toolbar.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Toolbar.module.css'
+
+import NaviItem from './Navi/NaviItem';
+
 
 
 const Toolbar = () => {
+
+	const [authHook, setAuthHook] = useState(false);
+
+	
+
 	return(
 			<div className={classes.toolbar}>
-				<p>Toolbar</p>
+				{authHook ? 
+				<>
+					<NaviItem text='Wyloguj'></NaviItem>
+					<NaviItem text='Dodaj Kolumne'></NaviItem>
+				</>
+				:
+				<>	
+					<NaviItem text='Zaloguj'></NaviItem>
+					<NaviItem text='Zarejestruj'></NaviItem>
+				</>
+				
+				}
 			</div>
 		);
 };
