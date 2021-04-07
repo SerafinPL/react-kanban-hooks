dde5652abf394b4ff7d7d101a1d80578488a08e1
serafinpl <ser.burberibar@gmail.com>
2021-04-07 17:01:24 +0200

    fetch the serwer login and registration to own hook

--- src/components/UI/Toolbar/Auth/Auth.js
+++ src/components/UI/Toolbar/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import classes from './Auth.module.css'
 
 import Input from '../../Input/Input';
@@ -15,62 +15,86 @@ const Auth = (props) => {
 	const [login, setLogin] = useState('');
 	const [passwd, setPasswd] = useState('');
 	const [passwd2, setPasswd2] = useState('');
+	const [valid, setValid] = useState(false);
 
-	const {sendAuth} = useRegLogHook();
+	const {sendAuth, error, token, time, email, userId, loading} = useRegLogHook();
+
+
+
+	useEffect(() => {
+		if (props.registration){
+			setValid(passwd === passwd2 && passwd.length > 5 && login !== '');
+		} else {
+			setValid(passwd.length > 5 && login !== '');
+		}
+	}, [passwd, passwd2, login])
+	
 
 
 	const singFunc = () => {
-		if (props.registration){ //Refinsration
-			const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCE9iNHzKkGA9SWX9TD4JvTXBEtyCxovdA';
-			const data = {
+
+		const data = {
 				email: login,
 				password: passwd,
 				returnSecureToken: true
 			}
-
+		if (props.registration){ //Refinsration
+			const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCE9iNHzKkGA9SWX9TD4JvTXBEtyCxovdA';
 			sendAuth(url, data);
 		} else { // Login
-
+			const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCE9iNHzKkGA9SWX9TD4JvTXBEtyCxovdA';
+			sendAuth(url, data);
 		}
 	}
 
-	return(
-	
-			<div className={classes.auth}>
-				<span onClick={props.clickX} className={classes.rightX}>&#9746;</span>
-				<p>Login:</p>
-				<Input 
-					value={login}
-					onChange={event => setLogin(event.target.value)}
-					type='text'
-				/>
-				<p>Hasło:</p>
-				<Input 
-					value={passwd}
-					onChange={event => setPasswd(event.target.value)}
-					type='password'
-				/>
-				{props.registration &&
-					<React.Fragment>
+	let display = (
+		<div className={classes.auth}>
+			{ !loading ?
+				<React.Fragment>	
+					<span onClick={props.clickX} className={classes.rightX}>&#9746;</span>
+					{error && <p style={{color: 'red'}}>Podano Błędne dane spróbuj jeszcze raz.</p>}
+					<p>Login:</p>
+					<Input 
+						value={login}
+						onChange={event => setLogin(event.target.value)}
+						type='text'
+					/>
+					<p>Hasło:</p>
+					<Input 
+						value={passwd}
+						onChange={event => setPasswd(event.target.value)}
+						type='password'
+					/>
+					{props.registration &&
+						<React.Fragment>
 
-						<p>Powtórz Hasło:</p>
-						<Input 
-							value={passwd2}				
-							onChange={event => setPasswd2(event.target.value)}
-							type='password'
-						/>
-						{passwd !== passwd2 &&
-							<p style={{color: 'red'}}>Hasła nie są identyczne</p>
-						}						
-					</React.Fragment>
-				}
-				<Button classes={'buttonGreen'}  click={singFunc} >{props.registration ? 'Zarejestruj' : 'Zaloguj'}</Button>
+							<p>Powtórz Hasło:</p>
+							<Input 
+								value={passwd2}				
+								onChange={event => setPasswd2(event.target.value)}
+								type='password'
+							/>
+							{passwd !== passwd2 &&
+								<p style={{color: 'red'}}>Hasła nie są identyczne</p>
 
-				
-			</div>
-			
+							}
+							{passwd.length < 6 &&
+								<p style={{color: 'red'}}>Minimum 6 znaków</p>
+							}						
+						</React.Fragment>
+					}
+					<Button classes={'buttonGreen'} click={singFunc} >{props.registration ? 'Zarejestruj' : 'Zaloguj'}</Button>
+				</React.Fragment>	
+			: <p>Oczekujemy na serwer!</p>}
 
+		</div>
 		);
+
+	if (userId) {
+		display = null; 
+	}
+
+	return display;
 };
 
 export default Auth;
