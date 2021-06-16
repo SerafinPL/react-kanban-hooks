6c28b566433d85f31a8023ed7385ddef7f9ba901
serafinpl <ser.burberibar@gmail.com>
2021-06-16 15:11:51 +0200

    transfer the context

--- src/App.js
+++ src/App.js
@@ -4,50 +4,53 @@ import "./App.css";
 import Toolbar from "./components/UI/Toolbar/Toolbar";
 import Kanban from "./containers/Kanban/Kanban";
 
-import FullContext from "./containers/context/context";
+import FullContextProvider from "./containers/context/context";
 
 const  App = () =>  {
-  const [isAuth, setIsAuth] = useState(false);
-  const [userId, setUserId] = useState(null);
-  const [token, setToken] = useState(null);
-  const [expirationDate, setExpirationDate] = useState(null);
-  const [email, setEmail] = useState(null);
+//   const [isAuth, setIsAuth] = useState(false);
+//   const [userId, setUserId] = useState(null);
+//   const [token, setToken] = useState(null);
+//   const [expirationDate, setExpirationDate] = useState(null);
+//   const [email, setEmail] = useState(null);
 
-  const loginOnHandler = (userId, token, expirationDate, email) => {
-    setIsAuth(true);
-    setUserId(userId);
-    setToken(token);
-    setExpirationDate(expirationDate);
-    setEmail(email);
-  };
+//   const loginOnHandler = (userId, token, expirationDate, email) => {
+//     setIsAuth(true);
+//     setUserId(userId);
+//     setToken(token);
+//     setExpirationDate(expirationDate);
+//     setEmail(email);
+//   };
 
-  const loginOffHandler = () => {
-    setIsAuth(false);
-    setUserId(null);
-    setToken(null);
-    setExpirationDate(null);
-    setEmail(null);
-  };
+//   const loginOffHandler = () => {
+//     setIsAuth(false);
+//     setUserId(null);
+//     setToken(null);
+//     setExpirationDate(null);
+//     setEmail(null);
+//   };
 
 
 
   return (
-    <FullContext.Provider
-      value={{
-        isAuth: isAuth,
-        userId: userId,
-        token: token,
-        expirationDate: expirationDate,
-        email: email,
-        loginOn: loginOnHandler,
-        loginOff: loginOffHandler,
-      }}
-    >
+    // <FullContext.Provider
+    //   value={{
+    //     isAuth: isAuth,
+    //     userId: userId,
+    //     token: token,
+    //     expirationDate: expirationDate,
+    //     email: email,
+    //     loginOn: loginOnHandler,
+    //     loginOff: loginOffHandler,
+    //   }}
+    // >
+	<FullContextProvider>
       <div className="App">
         <Toolbar />
         <Kanban />
       </div>
-    </FullContext.Provider>
+	</FullContextProvider>
+    // </FullContext.Provider>
+	
   );
 }
 
--- src/containers/context/context.js
+++ src/containers/context/context.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
-const context = React.createContext({
-  isAuth: false,
-  userId: null,
-  token: null,
-  expirationDate: null,
-});
+export const FullContext = React.createContext({});
 
-export default context;
+// eslint-disable-next-line
+export default (props) => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [userId, setUserId] = useState(null);
+  const [token, setToken] = useState(null);
+  const [expirationDate, setExpirationDate] = useState(null);
+  const [email, setEmail] = useState(null);
+
+  const loginOnHandler = (userId, token, expirationDate, email) => {
+    setIsAuth(true);
+    setUserId(userId);
+    setToken(token);
+    setExpirationDate(expirationDate);
+    setEmail(email);
+  };
+
+  const loginOffHandler = () => {
+    setIsAuth(false);
+    setUserId(null);
+    setToken(null);
+    setExpirationDate(null);
+    setEmail(null);
+  };
+
+  return (
+    <FullContext.Provider
+      value={{
+        isAuth: isAuth,
+        userId: userId,
+        token: token,
+        expirationDate: expirationDate,
+        email: email,
+        loginOn: loginOnHandler,
+        loginOff: loginOffHandler,
+      }}
+    >
+      {props.children}
+    </FullContext.Provider>
+  );
+};
